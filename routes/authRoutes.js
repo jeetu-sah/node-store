2f1e14a92d7f8b9364ca32e028499706cd6f2074
jeetaugurs <39190310+jeetaugurs@users.noreply.github.com>
2021-06-12 01:22:31 +0530

    registration complete and login page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,30 +1,79 @@
+'use strict';
 const express = require('express');
 const router = express.Router();
+const Joi = require('@hapi/joi');
 const { addUser } = require('../modules/users/service/userServices');
-const { registerSchema } = require('../modules/users/validation/registerValidation');
+const {joiFormatter , mongooseValidationFormator} = require('../helper/validationFormatter');
+const { authValidation }=  require('../modules/users/validation/authValidation');
 
 
+/**Login Request**/
+router.get('/login',(req , res)=>{
+    res.render('login',{
+                            status:200,
+                            error:{},
+                            formData:req.body
+                        })
+});
+
+/**
+ * 
+ * Login Post Request**/
+router.post('/login',(req , res)=>{
+    //return res.send(req.body);
+    res.render('login',{
+                            status:200,
+                            error:{},
+                            msg:"Login Successfully",
+                            formData:req.body
+                        })
+});
 
 
 /**
  * define route view file routes
  * **/
 router.get('/register' , (req , res) => {
-    res.render('register',{message:null})
+    res.render('register',{
+                            status:200,
+                            error:{},
+                            formData:req.body
+                        })
 });
 
 router.post('/register' , async (req , res)=>{
-    
+    const formData = req.body;
+   
+    //return res.send(req.body);
     try {
+        //form validation start and end
+        const validationResult = authValidation.validate(req.body ,{ abortEarly :false});
+        if(validationResult.error){
+            //return validation
+            return res.render('register',{
+                                status:300,
+                                error:joiFormatter(validationResult.error),
+                                formData:formData
+                                })
+        }
+        
+        
         const user = await addUser(req.body);
         return res.render('register',{
                                     message:"Registration Successfully.",
-                                    status:200 })
+                                    status:200,
+                                    validationErrorResponse:null,
+                                    formData:formData,
+                                    error:{}
+                                 })
         
     } catch (error) {
-        console.log(error)
+        const validationError =  mongooseValidationFormator(error);
         return res.render('register',{
-                                    message:"Something went wrong, please try again." ,status:100})
+                                    message:"Something went wrong, please try again." ,status:100,
+                                    validationErrorResponse:validationError,
+                                    formData:formData
+                                })
     }
   
 });
